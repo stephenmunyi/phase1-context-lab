5d99e2ee198e53619855bb591946ce86d8d90fa8
stephenmunyi <stephenmunyi075@gmail.com>
2024-04-15 18:29:06 +0300

    completed

--- index.js
+++ index.js
@@ -1,4 +1,224 @@
-git
+//git
+/* Your Code Here */
+
+/*
+ We're giving you this function. Take a look at it, you might see some usage
+ that's new and different. That's because we're avoiding a well-known, but
+ sneaky bug that we'll cover in the next few lessons!
+
+ As a result, the lessons for this function will pass *and* it will be available
+ for you to use if you need it!
+ */
+
+//  const createEmployeeRecord = function(row){
+//     return {
+//         firstName: row[0],
+//         familyName: row[1],
+//         title: row[2],
+//         payPerHour: row[3],
+//         timeInEvents: [],
+//         timeOutEvents: []
+//     }
+// };
+// console.log(createEmployeeRecord);
+
+// const createEmployeeRecords = function(employeeRowData) {
+//     return employeeRowData.map(function(row){
+//         return createEmployeeRecord(row)
+//     })
+// };
+// console.log(createEmployeeRecords);
+
+
+
+// const createTimeInEvent = function(dateStamp){
+//     let [date, hour] = dateStamp.split(' ')
+
+//     this.timeInEvents.push({
+//         type: "TimeIn",
+//         hour: parseInt(hour, 10),
+//         date,
+//     })
+
+//     return this
+// };
+// console.log(createTimeInEvent);
+
+// const createTimeOutEvent = function(dateStamp){
+//     let [date, hour] = dateStamp.split(' ')
+
+//     this.timeOutEvents.push({
+//         type: "TimeOut",
+//         hour: parseInt(hour, 10),
+//         date,
+//     })
+
+//     return this
+// };
+// console.log(createTimeOutEvent);
+
+// const hoursWorkedOnDate = function(soughtDate){
+//     let inEvent = this.timeInEvents.find(function(e){
+//         return e.date === soughtDate
+//     })
+
+//     let outEvent = this.timeOutEvents.find(function(e){
+//         return e.date === soughtDate
+//     })
+
+//     return (outEvent.hour - inEvent.hour) / 100
+// };
+// console.log(hoursWorkedOnDate);
+
+// const wagesEarnedOnDate = function(dateSought){
+//     let rawWage = hoursWorkedOnDate.call(this, dateSought)
+//         * this.payPerHour
+//     return parseFloat(rawWage.toString())
+// };
+// console.log(wagesEarnedOnDate);
+
+// const allWagesFor = function(){
+//     let eligibleDates = this.timeInEvents.map(function(e){
+//         return e.date
+//     })
+
+//     let payable = eligibleDates.reduce(function(memo, d){
+//         return memo + wagesEarnedOnDate.call(this, d)
+//     }.bind(this), 0)
+
+//     return payable
+// };
+// console.log(allWagesFor);
+
+// const findEmployeeByFirstName = function(srcArray, firstName) {
+//   return srcArray.find(function(rec){
+//     return rec.firstName === firstName
+//   })
+// };
+// console.log(findEmployeeByFirstName);
+
+// const calculatePayroll = function(arrayOfEmployeeRecords){
+//     return arrayOfEmployeeRecords.reduce(function(memo, rec){
+//         return memo + allWagesFor.call(rec)
+//     }, 0)
+// };
+// console.log(calculatePayroll);
+/* Your Code Here */
+
+/*
+ We're giving you this function. Take a look at it, you might see some usage
+ that's new and different. That's because we're avoiding a well-known, but
+ sneaky bug that we'll cover in the next few lessons!
+
+ As a result, the lessons for this function will pass *and* it will be available
+ for you to use if you need it!
+ */
+
+/* Your Code Here */
+
+/*
+ We're giving you this function. Take a look at it, you might see some usage
+ that's new and different. That's because we're avoiding a well-known, but
+ sneaky bug that we'll cover in the next few lessons!
+
+ As a result, the lessons for this function will pass *and* it will be available
+ for you to use if you need it!
+ */
+
+//  const createEmployeeRecord = function(row){
+//     return {
+//         firstName: row[0],
+//         familyName: row[1],
+//         title: row[2],
+//         payPerHour: row[3],
+//         timeInEvents: [],
+//         timeOutEvents: []
+//     }
+// };
+// console.log(createEmployeeRecord);
+
+// const createEmployeeRecords = function(employeeRowData) {
+//     return employeeRowData.map(function(row){
+//         return createEmployeeRecord(row)
+//     })
+// };
+// console.log(createEmployeeRecords);
+
+
+
+// const createTimeInEvent = function(dateStamp){
+//     let [date, hour] = dateStamp.split(' ')
+
+//     this.timeInEvents.push({
+//         type: "TimeIn",
+//         hour: parseInt(hour, 10),
+//         date,
+//     })
+
+//     return this
+// };
+// console.log(createTimeInEvent);
+
+// const createTimeOutEvent = function(dateStamp){
+//     let [date, hour] = dateStamp.split(' ')
+
+//     this.timeOutEvents.push({
+//         type: "TimeOut",
+//         hour: parseInt(hour, 10),
+//         date,
+//     })
+
+//     return this
+// };
+// console.log(createTimeOutEvent);
+
+// const hoursWorkedOnDate = function(soughtDate){
+//     let inEvent = this.timeInEvents.find(function(e){
+//         return e.date === soughtDate
+//     })
+
+//     let outEvent = this.timeOutEvents.find(function(e){
+//         return e.date === soughtDate
+//     })
+
+//     return (outEvent.hour - inEvent.hour) / 100
+// };
+// console.log(hoursWorkedOnDate);
+
+// const wagesEarnedOnDate = function(dateSought){
+//     let rawWage = hoursWorkedOnDate.call(this, dateSought)
+//         * this.payPerHour
+//     return parseFloat(rawWage.toString())
+// };
+// console.log(wagesEarnedOnDate);
+
+// const allWagesFor = function(){
+//     let eligibleDates = this.timeInEvents.map(function(e){
+//         return e.date
+//     })
+
+//     let payable = eligibleDates.reduce(function(memo, d){
+//         return memo + wagesEarnedOnDate.call(this, d)
+//     }.bind(this), 0)
+
+//     return payable
+// };
+// console.log(allWagesFor);
+
+// const findEmployeeByFirstName = function(srcArray, firstName) {
+//   return srcArray.find(function(rec){
+//     return rec.firstName === firstName
+//   })
+// };
+// console.log(findEmployeeByFirstName);
+
+// const calculatePayroll = function(arrayOfEmployeeRecords){
+//     return arrayOfEmployeeRecords.reduce(function(memo, rec){
+//         return memo + allWagesFor.call(rec)
+//     }, 0)
+// };
+// console.log(calculatePayroll);
+
 /* Your Code Here */
 
 /*
